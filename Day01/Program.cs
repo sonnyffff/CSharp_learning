8fe0d86f3eae409494c2b88ca02c5b36cb665379
sonny <1076659118@qq.com>
2024-09-13 18:27:53 +0800

    添加项目文件。

---
+++ Day01/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Day01
+{       
+    // class definition
+    class Program
+    {
+        // method definition
+        static void Main(string[] args)
+        {
+            
+            // attribute
+            Console.Title = "Hello World!";
+            
+            
+            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+
+            // 类和方法
+            Console.WriteLine(input);
+            Console.ReadLine(input);
+
+        }
+    }
+}
+
+
